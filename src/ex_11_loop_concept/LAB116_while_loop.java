fad9b00dd141cfd45bd95d385fd505dd07146108
Jayaraj Rajendran <jayaraj.rajendran@kovaion.com>
2025-04-02 00:26:12 +0530

    Initial commit

---
+++ src/ex_11_loop_concept/LAB116_while_loop.java
@@ -0,0 +1,14 @@
+package ex_11_loop_concept;
+
+public class LAB116_while_loop {
+    public static void main(String[] args) {
+
+
+        int i = 0;
+        while (i <10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
